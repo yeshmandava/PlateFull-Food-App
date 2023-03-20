859ea7993f9a160269b9c7868466cb3836722d06
amcgulapa <andreigulapa@knights.ucf.edu>
2023-03-19 20:23:52 -0400

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -99,7 +99,7 @@ function Login()
             <span id="inner-title">PLEASE LOG IN</span><br />
             <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
             <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
-            <input type="submit" id="loginButton" class="buttons" value = "Do It" onClick={doLogin} />
+            <input type="submit" id="loginButton" className="buttons" value = "Do It" onClick={doLogin} />
         </form>
         <span id="loginResult">{message}</span>
      </div>
