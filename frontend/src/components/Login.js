28c622ed480aa3481a87caffb817d06c5edf6820
Kevin Vu <vukevin0212@knights.ucf.edu>
2023-03-29 15:51:50 -0400

    animation in progress

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -45,7 +45,7 @@ function Login() {
           var user = { firstName: firstName, lastName: lastName, id: userId };
           // SaveCookie(firstName, lastName, userId);
           localStorage.setItem("user_data", JSON.stringify(user));
-          window.location.href = "/cards";
+          window.location.href = "/users";
         }
       })
       .catch(function (error) {
@@ -53,41 +53,9 @@ function Login() {
       });
   };
 
-  //     try
-  //     {
-  //         const response = await fetch(bp.buildPath('api/login'), {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-
-  //         var res = JSON.parse(await response.text());
-
-  //         if( res.id <= 0 )
-  //         {
-  //             setMessage('User/Password combination incorrect');
-  //         }
-  //         else
-  //         {
-  //             storage.storeToken(res);
-
-  //             let userId = res.id
-  //             let firstName = res.fn;
-  //             let lastName = res.ln;
-
-  //             let user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-  //             localStorage.setItem('user_data', JSON.stringify(user));
-
-  //             setMessage('');
-  //             window.location.href = '/cards';
-  //         }
-  //     }
-  //     catch(e)
-  //     {
-  //         alert(e.toString());
-  //         return;
-  //     }
-  // };
-
   return (
     <div id="loginDiv" className="login-card">
-      <form onSubmit={doLogin}>
+      <form onSubmit={doLogin}>s
         <span id="inner-title">PLEASE LOG IN</span>
         <br />
         <input
