e7dec5f076f27a1a39b07b14865c5b481dbb5e67
aterry5 <123323801+aterry5@users.noreply.github.com>
2023-03-30 16:33:33 -0400

    jwt update 1

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from "react";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { SaveCookie, ReadCookie } from "../components/Cookies";
 import '../stylesheets/Login.css'
 
