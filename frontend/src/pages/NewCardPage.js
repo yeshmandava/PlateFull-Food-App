96fc0b958b164f9ca3021446bbd4529b725ac3ee
Kevin Vu <vukevin0212@knights.ucf.edu>
2023-03-30 09:20:53 -0400

    added new page for adding recipes

---
+++ frontend/src/pages/NewCardPage.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import NewRecipe from '../components/NewRecipe';
+
+export default function NewCardPage(){
+    return(
+        <div>
+            <NewRecipe />
+        </div>
+    )
+}
