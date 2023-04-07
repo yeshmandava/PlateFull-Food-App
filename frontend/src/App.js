b976cb59b94ed216e42a8b2cb9e3f8b2c970a694
Kevin Vu <vukevin0212@knights.ucf.edu>
2023-04-07 19:02:32 -0400

    changes to routing addresses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import LoginPage from "./pages/LoginPage";
 import WelcomePage from "./pages/WelcomePage";
 import HomePage from "./pages/HomePage"
 import NewRecipePage from "./pages/NewRecipePage";
-import MyCookbook from "./pages/MyCookbook";
+import Cookbook from "./pages/Cookbook";
 
 function App() {
 	return (
@@ -16,8 +16,8 @@ function App() {
 				<Route path="/" index element={<WelcomePage />} />
 				<Route path="/login" index element={<LoginPage />} />
 				<Route path="/home" index element={<HomePage />} />
+				<Route path="/cookbook" index element={<Cookbook/>} />
 				<Route path="/new-recipe" index element={<NewRecipePage/>} />
-				<Route path="/my-cookBook" index element={<MyCookbook/>} />
 			</Routes>
 		</BrowserRouter>
 	);
